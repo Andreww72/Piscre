b9c2920c82153223540f4ab466a34a775e76b377
Andreww72 <12123941+Andreww72@users.noreply.github.com>
2021-08-31 18:14:54 +1000

    Better name

--- drawer.py
+++ drawer.py
@@ -2,8 +2,8 @@ import fetcher
 from read_env import read_env
 
 read_env()
-print(fetcher.get_coins())
-print(fetcher.get_news())
+#print(fetcher.get_coins())
+#print(fetcher.get_news())
 print(fetcher.get_wotd())
 print(fetcher.get_datetime())
 
